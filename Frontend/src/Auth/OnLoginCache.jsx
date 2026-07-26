1adb81434de245491a57be50ac6858db5f036482
AuGust R <ringelstettera4051@my.uwstout.edu>
2026-07-26 11:34:31 -0500

    - Moved Food Cache to Live in with Food Categories

--- Frontend/src/Auth/OnLoginCache.jsx
+++ Frontend/src/Auth/OnLoginCache.jsx
@@ -2,13 +2,13 @@ import { pullExercises } from "../ExercisesCard/PullExercise";
 import { pullPersonalExercises } from "../components/Cache/PersonalExerciseCache/PersonalExercise";
 import { pullTemplates } from "../components/Cache/TemplateCache/PullTemplate";
 import { pullWorkouts } from "../components/Cache/WorkoutCache/PullWorkout";
-import { pullFood } from "../components/Cache/FoodCache/PullUserFood";
-import { pullFood as pullAllFood } from "../components/Cache/FoodCache/PullFood";
+import { pullUserFood } from "../Food/PullUserFood.jsx";
+import { pullAllFood } from "../Food/PullAllFood.jsx";
 export function onLoginCache() {
   pullExercises().catch((err) => console.error("Exercise Cache error:", err));
   pullTemplates().catch((err) => console.error("Template Cache error:", err));
   pullWorkouts().catch((err) => console.error("Workout Cache error:", err));
   pullPersonalExercises().catch((err) => console.error("Personal Exercise Cache error:", err));
-  pullFood().catch((err) => console.error("Food Cache error:", err));
+  pullUserFood().catch((err) => console.error("Food Cache error:", err));
   pullAllFood().catch((err) => console.error("All Food Cache error:", err));
 }
